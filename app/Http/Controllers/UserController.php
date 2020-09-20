c192fe7eabab4843981305be6f59cd0b4c736e70
Ingrid Emilia <ingridemis1997@gmail.com>
2020-09-20 13:26:36 -0300

    commit teste

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+}
+
+
+/* O recurso de "clientes" deve conter:
+
+Primeiro nome
+Último nome
+E-mail */ 
